d9f1cc6aa02a0b8118aec093bfee343f8fa98d47
josuavndev <josuavnsia@gmail.com>
2026-09-11 15:00:40 +0700

    Fix Vercel 500 caused by missing view binding

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home')->name('home');
+// This page is static HTML. Avoid Route::view() because this deployment
+// intentionally does not register Laravel's view service provider.
+Route::get('/', function () {
+    return file_get_contents(resource_path('views/home.blade.php'));
+})->name('home');
