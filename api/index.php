604bda9aac8032af318d081edf8d0ae5f16f1b30
josuavndev <josuavnsia@gmail.com>
2026-09-11 14:48:55 +0700

    Use Laravel public front controller on Vercel

--- api/index.php
+++ api/index.php
@@ -1,13 +1,5 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-require __DIR__.'/../vendor/autoload.php';
-
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$app->handleRequest(Request::capture());
+// Vercel serverless entrypoint.
+// Hand the request to Laravel's normal public front controller.
+require __DIR__.'/../public/index.php';
