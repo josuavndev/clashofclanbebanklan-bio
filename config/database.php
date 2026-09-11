9c5916e8a83e82d53cea6643ed88900fe1cbac1d
josuavndev <josuavnsia@gmail.com>
2026-09-11 14:16:58 +0700

    fix Laravel runtime database config

---
+++ config/database.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'default' => env('DB_CONNECTION', 'sqlite'),
+    'connections' => [
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'url' => env('DB_URL'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],
+    ],
+    'migrations' => ['table' => 'migrations', 'update_date_on_publish' => true],
+    'redis' => ['client' => env('REDIS_CLIENT', 'phpredis'), 'options' => ['cluster' => env('REDIS_CLUSTER', 'redis'), 'prefix' => env('REDIS_PREFIX', '')]],
+];
