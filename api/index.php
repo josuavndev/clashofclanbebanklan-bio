f26872233f3da35437ba8863d1f09a2519636da5
josuavndev <josuavnsia@gmail.com>
2026-09-11 14:21:44 +0700

    Restore standard Laravel request entrypoint

--- api/index.php
+++ api/index.php
@@ -5,22 +5,9 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-try {
-    $autoload = __DIR__.'/../vendor/autoload.php';
-    if (!is_file($autoload)) {
-        throw new RuntimeException('Composer autoload not found: '.$autoload);
-    }
+require __DIR__.'/../vendor/autoload.php';
 
-    require $autoload;
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-    /** @var Application $app */
-    $app = require_once __DIR__.'/../bootstrap/app.php';
-    $app->handleRequest(Request::capture());
-} catch (Throwable $e) {
-    http_response_code(500);
-    header('Content-Type: text/plain; charset=utf-8');
-    echo "Laravel boot error\n\n";
-    echo get_class($e).": ".$e->getMessage()."\n";
-    echo "File: ".$e->getFile().":".$e->getLine()."\n\n";
-    echo $e->getTraceAsString();
-}
+$app->handleRequest(Request::capture());
